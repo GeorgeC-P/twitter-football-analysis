7ac538f0eb5394a88d7930b6a8c87e20a72adc46
george cooper-pearson <geoorge03@hotmail.com>
2017-03-30 22:52:23 +0100

    poking around at volume stuff over time

--- pandas_juicer.py
+++ pandas_juicer.py
@@ -1,7 +1,8 @@
 import pandas as pd
-import datetime
+import datetime, time
 import matplotlib.pyplot as plt
 from matplotlib import style
+
 style.use('ggplot')
 
 tweets_over_time = {}
@@ -10,21 +11,18 @@ volume = {}
 
 def twitter_sent_build_dicts(pos_sent, neg_sent, count):
     tweets_over_time[datetime.datetime.now()] = pos_sent + neg_sent
-    # print(tweets_over_time)
     volume[datetime.datetime.now()] = count
-    # print(volume)
-    if len(volume) % 5:
+    if len(volume) % 10 == 0:
         volume_analyser(volume)
 
 
 def volume_analyser(volume):
-    s = pd.Series(volume)
-    s.index.name='date'
-    print(s)
-    # if len(s) > 80:
-    #     s.plot()
-    #     plt.show()
-    #
+    vol_graph = pd.Series(volume)
+    vol_graph.index.name='date'
+    print (len(vol_graph))
+    vol_graph.plot()
+    plt.show()
+
 
 
 
