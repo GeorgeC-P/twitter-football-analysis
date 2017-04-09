1f17fc89e8bac36cd8dc06b1b5d12a945a32e517
george cooper-pearson <geoorge03@hotmail.com>
2017-04-09 22:52:51 +0100

    changes to the way the streamer works

--- pandas_juicer.py
+++ pandas_juicer.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import datetime
 import matplotlib.pyplot as plt
+import cufflinks as cf
 from matplotlib import style
 
-from football_sentiment import config
+from twitter_football_analysis import config
 
 tweets_over_time = []
 volume = {}
@@ -14,21 +15,24 @@ def twitter_sent_build_dicts(pos_sent, neg_sent, count):
     tweets_over_time.append((now, pos_sent, neg_sent, count))
     volume[datetime.datetime.now()] = count
     multi_graph(tweets_over_time)
-    if len(volume) % 10 == 0:
-        volume_analyser(volume)
+#     if len(volume) % 10 == 0:
+#         volume_analyser(volume)
+#
+#
+# def volume_analyser(volume):
+#     vol_graph = pd.Series(volume)
+#     vol_graph.index.name='date'
+#     print (len(vol_graph))
+#     vol_graph.plot()
+#     plt.show()
 
-
-def volume_analyser(volume):
-    vol_graph = pd.Series(volume)
-    vol_graph.index.name='date'
-    print (len(vol_graph))
-    vol_graph.plot()
-    plt.show()
 
 
 def multi_graph(tweets_over_time):
     labels = ['date_time', 'pos_sent', 'neg_sent', 'count']
     df = pd.DataFrame.from_records(tweets_over_time, columns=labels)
-    df.set_index('date_time', inplace = True)
-    if len(df) > 5:
-        df.iplot(secondary_y=['count'], filename='test_second_y')
+    df.set_index('date_time', inplace=True)
+    print(df)
+    if len(df) % 40  == 0:
+        df.iplot(secondary_y=['count'], filename='sentiment')
+        print('graph done')
--- sentiment_juicer.py
+++ sentiment_juicer.py
@@ -1,3 +1,9 @@
+from collections import Counter
+from nltk.tokenize import word_tokenize
+
 def tweet_analyse(tweet, sent):
-    print (tweet, sent)
+    count_all = Counter()
+    count_all.update(word_tokenize(tweet))
+    print(count_all.most_common(5))
+    print(sent)
     return True;
